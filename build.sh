c8fe43f098d2f3f402dd1efb71eb9dd07afc61e0
miya0001 <miya@wpist.me>
2016-04-13 23:39:50 +0900

    v4.5

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -ex
+
+curl -L https://wordpress.org/latest.tar.gz -o latest.tgz
+tar zxvf latest.tgz
+
+# action hooks
+find wordpress -name "*.php" | xargs grep "do_action" \
+| sed "s/^.*do_action/do_action/g"| sed "s/);.*/);/g" \
+| sed "s/ //g" | sed "s/\"/\'/g" | sed "s/do_action(\'//g" \
+| sed "s/\'.*$//g" | sed "s/^.*(.*$//g" | tr -d ' ' | sort | uniq > ./action-hooks.dict
+
+# filter hooks
+find . -name "*.php" | xargs grep "apply_filters" \
+| sed "s/^.*apply_filters/apply_filters/g"| sed "s/);.*/);/g" \
+| sed "s/ //g" | sed "s/\"/\'/g" | sed "s/apply_filters(\'//g" \
+| sed "s/\'.*$//g" | sed "s/^.*(.*$//g" | tr -d ' ' | sort | uniq > ./filter-hooks.dict
+
+# functions
+find . -name "*.php" -exec egrep '^function ' {} \; \
+| sed -e "s/function \(.*\)(.*/\1/g" | sed -e "s/^&//g" \
+| sed -e "s/(.*$//g" | tr -d ' ' |sort | uniq > ./functions.dict
+
+# class
+find . -name "*.php" | xargs grep "^class" \
+| sed "s/.*\:class //g" \
+| sed "s/ .*//g" | tr -d ' ' | sort | uniq > ./classes.dict
